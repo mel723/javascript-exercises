d4182694f58bdc35b78aa845aeaaa3f1309838c9
Melanie <183251709+mel723@users.noreply.github.com>
2025-04-01 19:27:24 +1100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(input) {
+  const inputArray = input.split("");
 
+  let output = ""
+
+  for (let i = input.length - 1; i >= 0; i--) {
+    output += inputArray[i];
+  }
+
+  return output;
 };
 
 // Do not edit below this line
