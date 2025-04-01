2d250276035d63ce4ecd9f595c3b72b720fcfd2f
Melanie <183251709+mel723@users.noreply.github.com>
2025-04-01 19:13:04 +1100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(input, numOfTimes) {
+  if (numOfTimes < 0) {
+    return "ERROR";
+  }
 
+  let output = "";
+
+  for (let i = 0; i < numOfTimes; i++) {
+    output += input;
+  }
+
+  return output;
 };
 
 // Do not edit below this line
