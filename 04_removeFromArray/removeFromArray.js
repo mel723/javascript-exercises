5384c3d81875e082c06bc41baaf3bd807e111a7c
Melanie <183251709+mel723@users.noreply.github.com>
2025-04-01 19:44:13 +1100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+  return inputArray.filter((input) => {
+    let keep = true;
+
+    for (const argument of args) {
+      if (input === argument) {
+        keep = false;
+        break;
+      }
+    }
+
+    return keep;
+  });
 };
 
 // Do not edit below this line
