3c481fd82aec4b3f51c7ef5def769f6c2a43ded6
Melanie <183251709+mel723@users.noreply.github.com>
2025-05-24 16:09:31 +1000

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+  const letters = sentence
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  return letters.split("").reverse().join("") === letters;
 };
 
 // Do not edit below this line
