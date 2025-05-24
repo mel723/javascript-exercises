2b704acc3487aae366dd8cc4bc0dac2808c0b9e7
Melanie <183251709+mel723@users.noreply.github.com>
2025-05-24 16:29:37 +1000

    Complete exercsie 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  const num = typeof input === "number" 
+    ? input 
+    : Number(input);
 
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  if (num === 0) {
+    return 0;
+  }
+
+  let arr = [1, 1];
+
+  while (arr.length < num) {
+    arr.push(arr.slice(-1)[0] + arr.slice(-2)[0]);
+  }
+
+  return arr.slice(-1)[0];
 };
 
 // Do not edit below this line
