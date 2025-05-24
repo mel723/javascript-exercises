566b20a486a54dd3653b53587e5cb566d26074f3
Melanie <183251709+mel723@users.noreply.github.com>
2025-05-24 16:42:40 +1000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const CURRENT_YEAR = 2025;
 
+const findTheOldest = function(people) {
+  const sortedPeople = people.toSorted((personA, personB) => {
+    const yearOfDeathA = personA.yearOfDeath === undefined 
+      ? CURRENT_YEAR 
+      : personA.yearOfDeath;
+    const yearOfDeathB = personB.yearOfDeath === undefined 
+      ? CURRENT_YEAR 
+      : personB.yearOfDeath;
+
+    const ageA = yearOfDeathA - personA.yearOfBirth;
+    const ageB = yearOfDeathB - personB.yearOfBirth;
+    return ageA - ageB;
+  })
+
+  return sortedPeople.slice(-1)[0];
 };
 
 // Do not edit below this line
